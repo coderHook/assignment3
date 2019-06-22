b5e421529f26f85afee4c18f11f232d56faf027d
coderhook <abel1987@hotmail.es>
2019-06-22 12:16:29 +0200

    Added data and a select option for each of it formatted from the data provided

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import './App.css';
 
+const data = [
+  {
+    name: "Ivel Z3",
+    manufacturer: "Ivasim",
+    year: 1969,
+    origin: "Croatia"
+  },
+  {
+    name: "Bally Astrocade",
+    manufacturer: "Bally Consumer Products",
+    year: 1977,
+    origin: "USA"
+  },
+  {
+    name: "Sord M200 Smart Home Computer",
+    manufacturer: "Sord Computer Corporation",
+    year: 1971,
+    origin: "Japan"
+  },
+  {
+    name: "Commodore 64",
+    manufacturer: "Commodore",
+    year: 1982,
+    origin: "USA"
+  }
+]
+
 function App() {
   return (
     <div className="App">
-
+      <select>
+        <option value="">-- pick a model --</option>
+        {
+          data.map( map => {
+            return <option value={map.name}>{map.name} ({map.year})</option>
+          })
+        }
+      </select>
     </div>
   );
 }
